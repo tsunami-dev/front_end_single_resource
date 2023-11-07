801ed94059e78a9da6f9b2a4a02bd65abafaf087
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-11-07 16:38:41 -0500

    entered the page elements code

---
+++ src/Pages/Edit.jsx
@@ -0,0 +1,12 @@
+import GamesEditForm from "../Components/ColorEditForm";
+
+function Edit() {
+  return (
+    <div className="new-edit">
+      <h2>Edit</h2>
+      <GameEditForm />
+    </div>
+  );
+}
+
+export default Edit;
